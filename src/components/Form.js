c36504fe88100f0396062c099d87d1fb91e730c7
Aman1101 <amanagrawal1101@gmail.com>
2019-04-18 00:26:01 -0700

    First Commit

---
+++ src/components/Form.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+
+const Form = props => (
+    <form onSubmit={props.getWeather}>
+                <input type="text" name="city" placeholder="City..."></input>
+                <input type="text" name="country" placeholder="Country..."></input>
+                <button>Get Weather</button>
+    </form>
+);
+
+// class Form extends React.Component{
+//     render(){
+//         return(
+//             <form onSubmit={this.props.getWeather}>
+//                 <input type="text" name="city" placeholder="City..."></input>
+//                 <input type="text" name="country" placeholder="Country..."></input>
+//                 <button>Get Weather</button>
+//             </form>
+//         );
+//     }
+// }       
+    
+export default Form; 
